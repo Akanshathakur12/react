2234207ec6706979d09426c458cdfa76b9d37fe6
nandan47 <80373995+nandan47@users.noreply.github.com>
2022-09-10 13:19:38 +0530

    Add files via upload

---
+++ component/Dashquery.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Query from './Query'
+import Card from './Card'
+
+export default function Dashquery() {
+  return (
+<div class="row">
+  <div class="col-md-8"><Card/></div>
+ 
+  
+  <div class="col-md-4"><Query/></div>
+  
+  
+ 
+</div>
+
+  )
+}
